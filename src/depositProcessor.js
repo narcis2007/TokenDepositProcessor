736ece080f827433a7d970cdad6a252297ec742a
narcis <narcis@creamcapital.io>
2019-11-11 16:58:56 +0200

    updates

--- src/depositProcessor.js
+++ src/depositProcessor.js
@@ -1,104 +1,118 @@
 const Sequelize = require("sequelize");
 const Web3 = require('web3');
 const sleep = require('system-sleep');
-const web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/3d1dacbcaeb34ea889ae105c15220e08'));
+const web3 = new Web3(new Web3.providers.HttpProvider(process.env.WEB3_PROVIDER));
+const Models = require('@narcis2007/ipour-models');
 
-const sequelize = new Sequelize(`postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
-    define: {
-        timestamps: false
-    }
-});
-// sequelize.sync();//{force:true}
+Models.syncDB(false);
 
-const Block = require("./models/block.js")(sequelize, Sequelize.DataTypes);
-const Deposit = require("./models/deposit.js")(sequelize, Sequelize.DataTypes);
-const User = require("./models/user.js")(sequelize, Sequelize.DataTypes);
-
-// User.sync({force:true});
+const Block = Models.Block;
+const Deposit = Models.Deposit;
+const User = Models.User;
+const Transfer = Models.Transfer;
 
 var tokenContract = new web3.eth.Contract(require('./ERC20ABI.json'), process.env.TOKEN_ADDRESS);
 
 
 async function processDepositConfirmations() {
-    while (true) {
-        var latestBlockNumber = (await web3.eth.getBlockNumber());
-        var confirmationBlockNumber = latestBlockNumber - 12;
 
-        var pendingDeposits = await Deposit.findAll({
-            where: {
-                blockNumber: {
-                    [Sequelize.Op.lt]: confirmationBlockNumber
-                }, confirmed: false
-            }
-        });
-        for (var i = 0; i < pendingDeposits.length; i++) {
-            if (latestBlockNumber - (await web3.eth.getTransaction(pendingDeposits[i].transactionHash)).blockNumber > 12) { //confirmed
-                pendingDeposits[i].confirmed = true;
-                pendingDeposits[i].save();
-                console.log(`deposit ${pendingDeposits[i].depositId} confirmed`)
-                var user = (await User.findByPk(pendingDeposits[i].userId));
-                user.balance += pendingDeposits[i].amount;
-                user.save();
-                console.log(`balance updated for user ${user.userId}, added ${pendingDeposits[i].amount}`)
-            }
+    var latestBlockNumber = (await web3.eth.getBlockNumber());
+    var confirmationBlockNumber = latestBlockNumber - 12;
+
+    var pendingDeposits = await Deposit.findAll({
+        where: {
+            blockNumber: {
+                [Sequelize.Op.lt]: confirmationBlockNumber
+            }, confirmed: false
         }
-        await sleep(process.env.SCAN_INTERVAL);
+    });
+    for (var i = 0; i < pendingDeposits.length; i++) {
+        if (latestBlockNumber - (await web3.eth.getTransaction(pendingDeposits[i].transactionHash)).blockNumber > 12) { //confirmed
+            pendingDeposits[i].confirmed = true;
+            pendingDeposits[i].save();
+
+            console.log(`deposit ${pendingDeposits[i].depositId} confirmed`)
+
+            var fee = 0;
+            var user = (await User.findByPk(pendingDeposits[i].userId));
+
+            if (user.hasCustomDepositFee) {
+                fee = pendingDeposits[i].amount * user.customDepositFee / 100;
+            } else {
+                fee = pendingDeposits[i].amount * process.env.DEPOSIT_FEE_PERCENTAGE / 100;
+            }
+
+            user.balance += (pendingDeposits[i].amount - fee);// TODO: add transfer event for external deposit
+            user.save();
+            console.log(`balance updated for user ${user.userId}`);
+
+            if (fee !== 0) {
+
+                var feeCollectorUser = (await User.findByPk(Models.Constants.FEE_COLLECTOR_USER_ID));
+                feeCollectorUser.balance += fee;
+                feeCollectorUser.save();
+                await Transfer.build({
+                    amount: fee,
+                    receiverId: feeCollectorUser.userId,
+                    senderId: pendingDeposits[i].userId
+                }).save();
+                console.log(`Fee collected from user ${user.userId}`);
+            }
+
+        }
     }
+
 }
 
 async function processDeposits() {
-    while (true) {
-        var latestBlockNumber = await web3.eth.getBlockNumber();
-        console.log('latestBlockNumber: ' + latestBlockNumber);
-        var fromBlock = parseInt((await Block.findByPk(1)).lastProcessedBlockNumber) + 1;
-        console.log('fromBlock: ' + fromBlock)
-        if (fromBlock <= latestBlockNumber) {
-            var hotWalletEvents = await tokenContract.getPastEvents('Transfer', {
-                // filter: {to: process.env.HOT_WALLET_ADDRESS},
-                fromBlock: fromBlock,
-                toBlock: latestBlockNumber
+
+    var latestBlockNumber = await web3.eth.getBlockNumber(); // TODO: use less await and let the processes run more in parallel
+    console.log('latestBlockNumber: ' + latestBlockNumber);
+    var fromBlock = parseInt((await Block.findByPk(Models.Constants.ETHEREUM_LAST_PROCESSED_BLOCK_ID)).lastProcessedBlockNumber) + 1;
+    console.log('fromBlock: ' + fromBlock)
+    if (fromBlock <= latestBlockNumber) {
+        var hotWalletEvents = await tokenContract.getPastEvents('Transfer', {
+            // filter: {to: process.env.HOT_WALLET_ADDRESS},
+            fromBlock: fromBlock,
+            toBlock: latestBlockNumber
+        });
+
+        for (var i = 0; i < hotWalletEvents.length; i++) {
+            var event = hotWalletEvents[i];
+            // TODO: if receiving address is in our DB add deposit
+
+
+            var users = await User.findAll({
+                where: {
+                    depositAddress: event.returnValues.to
+                }
             });
 
-            for (var i = 0; i < hotWalletEvents.length; i++) {
-                var event = hotWalletEvents[i];
-                // TODO: if receiving address is in our DB add deposit
-
-
-                var users = await User.findAll({
-                    where: {
-                        depositAddress: event.returnValues.to
-                    }
-                });
-
-                if (users.length === 1) {
-                    console.log(`Detected deposit to user ${users[0].userId}`);
-                    Deposit.build({
-                        amount: event.returnValues.value / 10 ** 8,
-                        transactionHash: event.transactionHash,
-                        blockNumber: event.blockNumber,
-                        userId: users[0].userId
-                    }).save();
-                }
-
-
+            if (users.length === 1) {
+                console.log(`Detected deposit to user ${users[0].userId}`);
+                Deposit.build({
+                    amount: event.returnValues.value / 10 ** 8,
+                    transactionHash: event.transactionHash,
+                    blockNumber: event.blockNumber,
+                    userId: users[0].userId
+                }).save();
             }
-
-            await Block.update(
-                {lastProcessedBlockNumber: latestBlockNumber},
-                {where: {blockID: 1}}
-            );
         }
 
-        await sleep(process.env.SCAN_INTERVAL);
+        await Block.update(
+            {lastProcessedBlockNumber: latestBlockNumber},
+            {where: {blockId: Models.Constants.ETHEREUM_LAST_PROCESSED_BLOCK_ID}}
+        );
     }
 };
 
-async function run() {
+function run() {
     try {
-
-        processDepositConfirmations();
-
-        processDeposits();
+        while (true) {
+            processDepositConfirmations();
+            processDeposits();
+            sleep(process.env.SCAN_INTERVAL);
+        }
 
     } catch (err) {
         console.log(err);
